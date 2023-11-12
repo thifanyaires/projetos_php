1df92302380a5af91d05a0e2e349f1c5054f9fdd
thifanyaires <thifaires@gmail.com>
2023-11-12 17:34:01 -0300

    estilizacao

--- caixa-eletronico/index.php
+++ caixa-eletronico/index.php
@@ -15,44 +15,48 @@
     $saque = $_REQUEST['saque'] ?? 0;
     $saque_valor = (int) $saque;
     ?>
-    <main role="main">
-        <div class="header">
-        <img src="./imagens/BITE-bank-logo.png" alt="Bite bank logo" id="logo">
+
+     
+    <section id="content">
+        <header class="header">
+            <img src="./imagens/bite-bank.png" alt="Bite bank logo" id="logo">
+        </header>        
+        <h1>CAIXA ELETRÔNICO</h1>
+
+        <div class="form">
+            <main role="main">
+                <form role="form" action="processar.php" method="get">
+                        <div id="saque">
+                            <label for="saque">DIGITE O VALOR QUE DESEJA SACAR</label>
+                            <br>
+                            <input type="number" name="saque" id="caixa_texto" step="1" required aria-label="Valor do saque em reais" aria-required="true" placeholder="R$ 0,00">
+                        </div>
+
+                        <div class="buttons">
+                            <button type="submit" value="sacar" id="sacar" class="button">Sacar</button>
+                            <button type="reset" value="limpar" id="limpar" class="button">Limpar</button>
+                        </div>
+                </form>        
+            </main>
         </div>
-        <h1 id="subtitle">CAIXA ELETRÔNICO</h1>
 
-        <form role="form" action="processar.php" method="get">
-            <section class="form">
-                <div id="saque">
-                    <label for="saque">Digite o valor que deseja sacar em R$</label>
-                    <br>
-                    <input type="number" name="saque" id="saque" step="1" required aria-label="Valor do saque em reais" aria-required="true">
-                </div>
-
-                <div class="buttons">
-                <button type="submit" value="sacar" id="sacar">Sacar</button>
-                <button type="reset" value="limpar" id="limpar">Limpar</button>
-            </div>
-                
-                    <h2>Notas disponíveis para saque:</h2>
-                
-                <div id="notas">
-                    <img class="nota" src="imagens/notas/200-reais.jpg" alt="200 reais">
-                    
-                    <img class="nota" src="imagens/notas/100-reais.jpg" alt="100 reais">
-                    
-                    <img class="nota" src="imagens/notas/50-reais.jpg" alt="50 reais">
-                    
-                    <img class="nota" src="imagens/notas/20-reais.jpg" alt="20 reais">
-                    
-                    <img class="nota" src="imagens/notas/10-reais.jpg" alt="10 reais">
-                    
-                    <img class="nota" src="imagens/notas/5-reais.jpg" alt="5 reais">
-                    
-                    <img class="nota" src="imagens/notas/2-reais.jpg" alt="2 reais">
-                </div>
-            </section>    
-        </form>
-    </main>
+        <div id="notas">
+            <h2 id="disponivel">NOTAS DISPONÍVEIS:</h2>
+                        
+                <img class="nota" src="imagens/notas/200-reais.jpg" alt="200 reais">
+                            
+                <img class="nota" src="imagens/notas/100-reais.jpg" alt="100 reais">
+                            
+                <img class="nota" src="imagens/notas/50-reais.jpg" alt="50 reais">
+                            
+                <img class="nota" src="imagens/notas/20-reais.jpg" alt="20 reais">
+                            
+                <img class="nota" src="imagens/notas/10-reais.jpg" alt="10 reais">
+                            
+                <img class="nota" src="imagens/notas/5-reais.jpg" alt="5 reais">
+                            
+                <img class="nota" src="imagens/notas/2-reais.jpg" alt="2 reais">
+        </div>
+    </section>        
 </body>
 </html>
--- caixa-eletronico/processar.php
+++ caixa-eletronico/processar.php
@@ -12,7 +12,7 @@ $saque = $_GET['saque'] ?? 0;
 $saque_valor = (int) $saque;
 
 if ($saque > $saldo) {
-    echo '<div style="margin-bottom: 20px;"><b style="font-size: 24px;">Saldo insuficiente para esta operação. Seu saldo atual é de: ' . $saldo . '</b></div>';
+    echo '<div id="saldo-insuficiente">Saldo insuficiente para esta operação.<br/><br/> Seu saldo atual é de: ' . $saldo . ',00</b></div>';
 } else {
    
     $saldo_anterior = $saldo;
@@ -49,55 +49,65 @@ if ($saque > $saldo) {
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<link rel="icon" type="image/png" href="./imagens/icons/icon.png">
-<link rel="stylesheet" href="style.css">   
+    <link rel="icon" type="image/png" href="./imagens/icons/icon.png">
+    <link rel="stylesheet" href="style.css">   
 </head>
 <body>
-    
-
-    <main role="alert" aria-live="assertive" class="body">
     <?php if ($saque <= $saldo) { ?>
-            <h2>Saque de R$<?=$saque_valor?> realizado</h2>
+    
+    <section id="content">
+        <header class="header">
+            <img src="./imagens/bite-bank.png" alt="Bite bank logo" id="logo">
+        </header>
+                
+        <h1>SAQUE REALIZADO COM SUCESSO</h1>
+
+        <main role="alert" aria-live="assertive">
+            <h2>Você sacou R$<?=$saque_valor?>,00</h2>
+                
             <p>O caixa eletrônico vai te entregar as seguintes notas:</p>
+            
             <ul id="notas">
-                <?php if ($tot200 > 0) { ?>
-                    <li><img src="imagens/notas/200-reais.jpg" alt="Nota de 200" class="nota" aria-hidden="true"> x <?=$tot200?></li>
-                <?php } ?>
-                <?php if ($tot100 > 0) { ?>
-                    <li><img src="imagens/notas/100-reais.jpg" alt="Nota de 100" class="nota" aria-hidden="true"> x <?=$tot100?></li>
-                <?php } ?>
-                <?php if ($tot50 > 0) { ?>
-                    <li><img src="imagens/notas/50-reais.jpg" alt="Nota de 50" class="nota" aria-hidden="true"> x <?=$tot50?></li>
-                <?php } ?>
-                <?php if ($tot20 > 0) { ?>
-                    <li><img src="imagens/notas/20-reais.jpg" alt="Nota de 20" class="nota" aria-hidden="true"> x <?=$tot20?></li>
-                <?php } ?>
-                <?php if ($tot10 > 0) { ?>
-                    <li><img src="imagens/notas/10-reais.jpg" alt="Nota de 10" class="nota" aria-hidden="true"> x <?=$tot10?></li>
-                <?php } ?>
-                <?php if ($tot5 > 0) { ?>
-                    <li><img src="imagens/notas/5-reais.jpg" alt="Nota de 5" class="nota" aria-hidden="true"> x <?=$tot5?></li>
-                <?php } ?>
-                <?php if ($tot2 > 0) { ?>
-                    <li><img src="imagens/notas/2-reais.jpg" alt="Nota de 2" class="nota" aria-hidden="true"> x <?=$tot2?></li>
-                <?php } ?>
+                    <?php if ($tot200 > 0) { ?>
+                        <li><img src="imagens/notas/200-reais.jpg" alt="Nota de 200" class="nota" aria-hidden="true"><br/> x <?=$tot200?></li>
+                    <?php } ?>
+                    <?php if ($tot100 > 0) { ?>
+                        <li><img src="imagens/notas/100-reais.jpg" alt="Nota de 100" class="nota" aria-hidden="true"><br/> x <?=$tot100?></li>
+                    <?php } ?>
+                    <?php if ($tot50 > 0) { ?>
+                        <li><img src="imagens/notas/50-reais.jpg" alt="Nota de 50" class="nota" aria-hidden="true"><br/> x <?=$tot50?></li>
+                    <?php } ?>
+                    <?php if ($tot20 > 0) { ?>
+                        <li><img src="imagens/notas/20-reais.jpg" alt="Nota de 20" class="nota" aria-hidden="true"><br/> x <?=$tot20?></li>
+                    <?php } ?>
+                    <?php if ($tot10 > 0) { ?>
+                        <li><img src="imagens/notas/10-reais.jpg" alt="Nota de 10" class="nota" aria-hidden="true"><br/> x <?=$tot10?></li>
+                    <?php } ?>
+                    <?php if ($tot5 > 0) { ?>
+                        <li><img src="imagens/notas/5-reais.jpg" alt="Nota de 5" class="nota" aria-hidden="true"><br/> x <?=$tot5?></li>
+                    <?php } ?>
+                    <?php if ($tot2 > 0) { ?>
+                        <li><img src="imagens/notas/2-reais.jpg" alt="Nota de 2" class="nota" aria-hidden="true"><br/> x <?=$tot2?></li>
+                    <?php } ?>
             </ul>
-            <p>
-                <h2>Seu saldo anterior era de: R$<?=$saldo_anterior?></h2>
-                <h2>Seu saldo final em reais ficou em: R$<?=$saldo_final?></h2>
-            </p>
-        <?php } ?>
 
-        <button onclick="window.history.back()" class="buttons" id="return">Voltar</button>
+            <div id="saldo">
+                <h3>Seu saldo anterior era de: R$<?=$saldo_anterior?>,00</h3>
+                <h3>Seu saldo final ficou em: R$<?=$saldo_final?>,00</h3>
+            </div>
+            <?php } ?>
+        </main>
+            
+        <button onclick="window.history.back()" id="return" class="button">Voltar</button>
+    </section>    
 
-        <script>
-            var saqueValor = "<?=$saque_valor?>";
-            var saldoFinal = "<?=$saldo_final?>";
-            var mensagem = "Saque de " + saqueValor + " Reais realizado. Seu saldo é de: " + saldoFinal;
-            var synth = window.speechSynthesis;
-            var utterThis = new SpeechSynthesisUtterance(mensagem);
-            synth.speak(utterThis);
-        </script>
-    </main>
+    <script>
+        var saqueValor = "<?=$saque_valor?>";
+        var saldoFinal = "<?=$saldo_final?>";
+        var mensagem = "Saque de " + saqueValor + " Reais realizado. Seu saldo é de: " + saldoFinal;
+        var synth = window.speechSynthesis;
+        var utterThis = new SpeechSynthesisUtterance(mensagem);
+        synth.speak(utterThis);
+    </script>
 </body>
 </html>
